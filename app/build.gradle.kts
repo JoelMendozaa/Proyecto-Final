52648055291c2f7f0724131bd526a93e088ff95f
Yowi <yowismr@gmail>
2024-04-03 13:25:00 +0100

    Upgradle Update
    Se han añadido los gradles de Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
     id("kotlin-kapt")
     // Google Firebase
 //    id("com.android.application")
-//    id("com.google.gms.google-services")
-}
+    id("com.google.gms.google-services")}
 
 android {
     namespace = "com.example.gestorinventarioinformaticali"
@@ -92,6 +91,6 @@ dependencies {
     implementation("io.coil-kt:coil-compose:1.4.0")
 
     //Google
-//    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-//    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
